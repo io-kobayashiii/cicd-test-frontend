8ab56803758cd870b3eac7e07606bb84845cdfba
io-kobayashiii <io.iobayashiii@gmail.com>
2024-07-25 21:30:13 +0900

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState('Before fetching');
+  useEffect(() => {
+    fetch('/api/health')
+      .then((response) => {
+        setResult(JSON.stringify(response));
+      })
+      .catch((error) => {
+        setResult(JSON.stringify(error));
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>iii-playground</h1>
+      <p>Hello World.</p>
+      <h2>Fetch Result</h2>
+      <p>{result}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
